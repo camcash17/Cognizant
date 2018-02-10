06edfabd900ae8241419199a4b2d60332552a4c9
Cameron Casher <camcash714@gmail.com>
2018-02-09 23:04:58 -0500

    add sequelize variable

--- index.js
+++ index.js
@@ -11,18 +11,44 @@ Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
+  // const Sequelize = require('sequelize');
+  const sequelize = new Sequelize('database', 'null', 'null', {
+    host: 'localhost',
+    dialect: 'sqlite',
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+
+    // SQLite only
+    storage: './db/database.db'
+  });
+
 // ROUTES
 
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
-    console.log('body:', body);
-    res.json(body);
-  })
-  // res.status(500).send('Not Implemented');
+  // request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
+  //   console.log('body:', body);
+  //   res.send(body);
+  sequelize.query("SELECT * FROM `films`", { type: sequelize.QueryTypes.SELECT})
+
+    .then(films => {
+      // We don't need spread here, since only the results will be returned for select queries
+      res.json(films)
+    })
+    .catch(err => {
+      res.status(500).send('Not Implemented');
+    })
+  // })
 }
+
+
 
 
 
