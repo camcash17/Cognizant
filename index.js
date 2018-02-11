f1638c9fbe639897010ef34c397ee341911c35ab
Cameron Casher <camcash714@gmail.com>
2018-02-11 10:37:53 -0500

    updated meta

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const sqlite = require('sqlite'),
       Sequelize = require('sequelize'),
       request = require('request'),
       express = require('express'),
+      axios = require('axios');
       app = express();
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
@@ -11,7 +12,6 @@ Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
-  // const Sequelize = require('sequelize');
   const sequelize = new Sequelize('database', 'null', 'null', {
     host: 'localhost',
     dialect: 'sqlite',
@@ -23,7 +23,6 @@ Promise.resolve()
       idle: 10000
     },
 
-    // SQLite only
     storage: './db/database.db'
   });
 
@@ -59,73 +58,61 @@ app.get('/films/:id/recommendations/notarealroute', (req, res) => {
   })
 });
 
-// app.get('/films/notanid/recommendations', (req, res) => {
-//   res.status(422).json({
-//     message: 'Key Missing'
-//   })
-// });
-//
-// app.get('/films/notanid/recommendations?offset=notanoffset&limit=notalimit', (req, res) => {
-//   res.status(422).json({
-//     message: 'Key Missing'
-//   })
-// });
-
 // ROUTE HANDLER
 
   function getFilmRecommendations(req, res) {
     // request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
     //   console.log('body:', response);
     //   res.send(body);
+    // axios(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`)
+    // .then((res) => {
+    //   let reviews = res.data;
+    //   console.log("film id", reviews[0].reviews[0].id)
 
-    let plus15;
-    let minus15;
-    sequelize.query(`SELECT * FROM films WHERE id == ${req.params.id}`, { type: sequelize.QueryTypes.SELECT})
+      let plus15;
+      let minus15;
 
+      sequelize.query(`SELECT * FROM films WHERE id == ${req.params.id}`, { type: sequelize.QueryTypes.SELECT})
       .then(chosenFilm => {
         plus15 = chosenFilm[0].release_date.split('-');
         minus15 = chosenFilm[0].release_date.split('-');
         plus15[0] = (parseInt(plus15[0])+15).toString();
         minus15[0] = (parseInt(minus15[0])-15).toString();
-
         console.log("id", plus15.join('-'));
 
-        // We don't need spread here, since only the results will be returned for select queries
         sequelize.query(`SELECT * FROM films WHERE genre_id == ${chosenFilm[0].genre_id} AND release_date >= '${minus15.join('-')}' AND release_date <= '${plus15.join('-')}'`, { type: sequelize.QueryTypes.SELECT})
-
-          .then(films => {
-            console.log("films", films);
-            sequelize.query(`SELECT name FROM genres WHERE genres.id == ${films[0].genre_id}`, { type: sequelize.QueryTypes.SELECT})
-            // console.log("data", films);
-            // We don't need spread here, since only the results will be returned for select queries
-            .then(genre => {
-              console.log("id", films.length);
-              // films.map((d, i) => {
-                res.send({
-                  recommendations: [
-                    {
-                      id: films[0].id,
-                      title: films[0].title,
-                      release_date: films[0].release_date,
-                      genre: genre[0].name,
-                      averageRating: null,
-                      reviews: null
-                    }
-                  ],
-                  "meta": {
-                     "limit": 10,
-                     "offset": 0
+        .then(films => {
+          console.log("films", films);
+          sequelize.query(`SELECT name FROM genres WHERE genres.id == ${films[0].genre_id}`, { type: sequelize.QueryTypes.SELECT})
+          // console.log("data", films);
+          .then(genre => {
+            console.log("id", films.length);
+            // Films.findAll().then(recommendations => {
+              res.json({
+                recommendations: [
+                  {
+                    id: films[0].id,
+                    title: films[0].title,
+                    release_date: films[0].release_date,
+                    genre: genre[0].name,
+                    averageRating: null,
+                    reviews: null
                   }
-                })
-              // })
-            })
+                ],
+                meta: {
+                   limit: 10,
+                   offset: 0
+                }
+              })
+            // })
           })
-          .catch(err => {
-            res.status(422).json({
-              message: 'Key Missing'
-            })
-            // res.status(500).send('Not Implemented');
+        })
+        .catch(err => {
+          res.status(422).json({
+            message: 'Key Missing'
           })
+          // res.status(500).send('Not Implemented');
+        })
       })
       .catch(err => {
         res.status(422).json({
